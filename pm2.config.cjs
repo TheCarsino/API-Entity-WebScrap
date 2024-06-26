0fbe5cff5516895911eb4a5073362edea14d916c
Lucas Podestá <78005968+TheCarsino@users.noreply.github.com>
2024-06-25 19:32:19 -0500

    v1.1 - FIX for Deploy
    Configuration of the pm2

---
+++ pm2.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "server", // Replace with your application name
+      script: "npm run start", // Replace with your actual start script
+      env: {
+        // Define environment variables here if needed
+      },
+    },
+  ],
+};
